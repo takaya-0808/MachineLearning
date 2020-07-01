083b6680c41043a18a0360a04b0cebc81bdd1f13
Takaya <takaya1912@gmail.com>
2020-07-02 00:54:59 +0900

    サンプル2

---
+++ work/課題5/sample2.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import pulp
+
+def main():
+    #線形計画の定義
+    problem = pulp.LpProblem('problem', pulp.LpMaximize)
+
+    #変数の定義
+    x1 = pulp.LpVariable('x1', lowBound = 0) 
+    x2 = pulp.LpVariable('x2', lowBound = 0)
+    x3 = pulp.LpVariable('x3', lowBound = 0)
+    x4 = pulp.LpVariable('x4', lowBound = 0) 
+    x5 = pulp.LpVariable('x5', lowBound = 0) 
+
+    #目的関数の定義
+    problem += 2*x1 + x2
+
+    #制約条件の定義
+    problem += x1 + 2*x2 + 2*x3 == 14
+    problem += x1 + x2 + x4 == 8
+    problem += 3*x1 + x2 + x5 == 18
+
+    #解く
+    status = problem.solve()
+    print(pulp.LpStatus[status])
+
+    #結果表示
+    print("Result")
+    print("x1:",x1.value())
+    print("x2:",x2.value())
+    print("x3:",x3.value())
+    print("x4:",x4.value())
+    print("x5:",x5.value())
+    print("problem:",problem.objective.value())
+    
+if __name__ == "__main__":
+    main()
