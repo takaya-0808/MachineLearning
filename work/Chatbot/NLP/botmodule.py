c51bde405e2cbbe01eaf4d402b6e450cf5f6b91b
Takaya <takaya1912@gmail.com>
2020-07-11 11:10:17 +0900

    他channel にメッセージを送る

--- work/Chatbot/NLP/botmodule.py
+++ work/Chatbot/NLP/botmodule.py
@@ -1,7 +1,12 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 import csv
+from slacker import Slacker
+import slackbot_settings
+import requests
+import json
 
+"""
 @respond_to("こんにちわ")
 def greeting_1(messge):
     messge.reply("こんにちわ!")
@@ -10,22 +15,43 @@ def greeting_1(messge):
 def greeting_2(messge):
     messge.reply("コンニチワ")
 
-
 @listen_to("くんにちわ")
 def greeting_3(messge):
     messge.reply("ボクはキリンです")
+"""
 
 @respond_to("Give me (.*)")
 def Create_nlp_tasks(message, params):
-    #channel_id = message.channel._body("name")
-    #print(channel_id)
-    text = message.body["text"]
-    options = []
-    for i,v in enumerate(params):
-        options.append
+
     send_user = message.channel._client.users[message.body['user']][u'name']
-    print(text)
-    message.send("Your message is {}".format(params))
     args = [row for row in csv.reader([params], delimiter='　')][0]
-    message.send(args[0])
-    print(args)
+    
+    post_url = "https://slack.com/api/chat.postMessage"
+    channel = "# general"
+    msg = [
+        {
+            "color": "#3104B4",
+            "fields": [
+                {
+                    "title": "担当者名",
+                    "value": send_user
+                },
+                {
+                    "title": "タスク・分野名",
+                    "value": args[0]
+                },
+                {
+                    "title": "時間",
+                    "value": args[1]
+                }
+            ]
+        }
+    ]
+
+    payload = {
+        "token": slackbot_settings.API_TOKEN,
+        "channel": channel,
+        "attachments": json.dumps(msg)
+    }
+    res = requests.post(post_url, data=payload)
+    #message.send_webapi("NLPタスク研:", json.dumps(msg))
